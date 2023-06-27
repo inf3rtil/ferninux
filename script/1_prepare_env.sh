e9bcaacee910947d23f57f50f6a1a7484433b60f
Fernando Ferreira <fernando@ferreirasil.com>
2023-06-26 21:33:53 -0300

    Create virtual disk image
     prepare vdisk

---
+++ script/1_prepare_env.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -e
+
+WORK_DIR=$PWD/../../../temp
+WGET_FILE=$PWD/wget-list-systemd
+cd $WORK_DIR
+LFS=$PWD/lfs
+VDISK_SIZE_GB=5
+VDISK_FILENAME=ferninux.img
+VDISK_LABEL=gpt
+VDISK_ROOT_PART=p2
+
+
+
+
+echo "creating LFS:($LFS) dir"
+mkdir -pv $WORK_DIR
+mkdir -pv $LFS
+
+cd $LFS
+if test -f "$VDISK_FILENAME"; then
+    echo "$VDISK_FILENAME found"
+    echo "mounting loop device"
+    disk_loop=$(losetup --partscan --show --verbose --find $VDISK_FILENAME)
+else
+    echo "$VDISK_FILENAME not found"
+    echo "creating virtual disk image: $VDISK_FILENAME file"
+    dd if=/dev/zero of=$VDISK_FILENAME bs=1G count=$VDISK_SIZE_GB status=progress
+    
+    echo "creating $VDISK_LABEL label on $VDISK_FILENAME"
+    parted -s $VDISK_FILENAME mklabel $VDISK_LABEL
+    echo "creating boot partition on $VDISK_FILENAME"
+    parted -s $VDISK_FILENAME mkpart boot fat32 1Mib 512Mib
+    echo "creating root partition on $VDISK_FILENAME"
+    parted -s $VDISK_FILENAME mkpart root ext4 512Mib 100%
+    echo "set esp flag on boot partition"
+    parted -s $VDISK_FILENAME set 1 esp on
+    disk_loop=$(losetup --partscan --show --verbose --find $VDISK_FILENAME)
+    root_part=$disk_loop$VDISK_ROOT_PART
+    echo "format boot partition as FAT32"
+    mkfs.vfat -v -F32 $disk_loop\p1
+    echo "formate root partition as ext4"
+    mkfs.ext4 -v $root_part
+fi
+
+echo "mounting root partition"
+mount -v -t ext4 "$disk_loop"p2 $LFS
+
+mkdir -pv $LFS/sources
+chmod -v a+wt $LFS/sources
+
+wget --input-file=$WGET_FILE --continue --directory-prefix=$LFS/sources
+
+echo "cleaning"
+umount -v "$disk_loop"p2
+losetup --verbose -d $disk_loop
+
+
