c621b3d17a73624ab207fdc2623d13ca9b753f06
Fernando Ferreira Silva <ffs@fer.dev.br>
2024-02-07 21:07:10 -0300

    fix template recipe

--- util/template_recipe.sh
+++ util/template_recipe.sh
@@ -3,8 +3,9 @@
 PACKAGE_NAME=
 VERSION=
 DOWNLOAD_URL=
-SRC_COMPRESSED_FILE=$(echo $url | rev | cut -d '/' -f 1 | rev)
-SRC_FOLDER=$PACKAGE_NAME$VERSION
+MD5_SUM=
+SRC_COMPRESSED_FILE=$(echo $DOWNLOAD_URL | rev | cut -d '/' -f 1 | rev)
+SRC_FOLDER=$PACKAGE_NAME-$VERSION
 
 build_source_package(){
 
