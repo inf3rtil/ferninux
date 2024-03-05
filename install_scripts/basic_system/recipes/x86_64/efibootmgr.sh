7573680104a0b1e3f55c3695d88613cbb3d2cf24
Fernando Ferreira Silva <fernando@fer.dev.br>
2024-03-04 21:16:33 -0300

    remove old recipes

--- install_scripts/basic_system/recipes/x86_64/efibootmgr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SRC_COMPRESSED_FILE=efibootmgr-18.tar.gz
-SRC_FOLDER=efibootmgr-18
-
-build_source_package(){
-    make EFIDIR=LFS EFI_LOADER=grubx64.efi
-    make install EFIDIR=LFS
-}
--- install_scripts/basic_system/recipes/x86_64/iana-etc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SRC_COMPRESSED_FILE=iana-etc-20230810.tar.gz
-SRC_FOLDER=iana-etc-20230810
-
-build_source_package(){
-    cp services protocols /etc
-}
-
