b29189fae7926c781df1052b4e06c67ceda02d6b
Fernando Ferreira Silva <fernando@fer.dev.br>
2025-01-19 01:17:57 -0300

    remove version from temp_tools recipes title

--- util/env/enter_chroot.sh
+++ util/env/enter_chroot.sh
@@ -11,66 +11,68 @@ if [[ $(whoami) != "root" ]]; then
     exit 1
 fi
 
-mount_devices
 
-chown -R root:root $LFS/{usr,lib,var,etc,bin,sbin,tools}
-case $(uname -m) in
-    x86_64) chown -R root:root $LFS/lib64 ;;
-esac
+enter_chroot() {
+    mount_devices
+    chown -R root:root $LFS/{usr,lib,var,etc,bin,sbin,tools}
+    case $(uname -m) in
+	x86_64) chown -R root:root $LFS/lib64 ;;
+    esac
 
-mkdir -pv $LFS/{dev,proc,sys,run}
+    mkdir -pv $LFS/{dev,proc,sys,run}
 
-# install ferninux scripts to run inside chrooted env
-mkdir -pv $LFS/script
-cp -pr $CHROOT_SCRIPTS_DIR/* $LFS/script/
+    # install ferninux scripts to run inside chrooted env
+    mkdir -pv $LFS/script
+    cp -pr $CHROOT_SCRIPTS_DIR/* $LFS/script/
 
-mount -v --bind /dev $LFS/dev
-mount -v --bind /dev/pts $LFS/dev/pts
-mount -vt proc proc $LFS/proc
-mount -vt sysfs sysfs $LFS/sys
-mount -vt tmpfs tmpfs $LFS/run
+    mount -v --bind /dev $LFS/dev
+    mount -v --bind /dev/pts $LFS/dev/pts
+    mount -vt proc proc $LFS/proc
+    mount -vt sysfs sysfs $LFS/sys
+    mount -vt tmpfs tmpfs $LFS/run
 
-if [ -h $LFS/dev/shm ]; then
-  mkdir -pv $LFS/$(readlink $LFS/dev/shm)
-else
-  mount -t tmpfs -o nosuid,nodev tmpfs $LFS/dev/shm
-fi
+    if [ -h $LFS/dev/shm ]; then
+	mkdir -pv $LFS/$(readlink $LFS/dev/shm)
+    else
+	mount -t tmpfs -o nosuid,nodev tmpfs $LFS/dev/shm
+    fi
 
-findmnt | grep $LFS
+    findmnt | grep $LFS
 
-# select a autorun script if AUTOINSTALL is set
-if [[ $AUTOINSTALL -eq 1 ]]; then
-    chroot_script="/script/ferninux.sh"
-fi
+    # select a autorun script if AUTOINSTALL is set
+    if [[ $AUTOINSTALL -eq 1 ]]; then
+	chroot_script="/script/ferninux.sh"
+    fi
 
-if [[ $INSTALL_KERNEL -eq 1 ]]; then
-    chroot_script="/script/kernel/build_linux_kernel.sh"
-fi
+    if [[ $INSTALL_KERNEL -eq 1 ]]; then
+	chroot_script="/script/kernel/build_linux_kernel.sh"
+    fi
     
-chroot "$LFS" /usr/bin/env -i \
-       HOME=/root \
-       TERM="$TERM" \
-       PS1='(lfs chroot) \u:\w\$ ' \
-       PATH=/usr/bin:/usr/sbin \
-       MAKEFLAGS=$MAKEFLAGS \
-       USE_UEFI=$USE_UEFI \
-       FERNINUX_TARGET_ARCH=$FERNINUX_TARGET_ARCH \
-       INSTALLED_PACKAGES_FILE=$INSTALLED_PACKAGES_FILE \
-       FERNINUX_KERNEL_VERSION=$FERNINUX_KERNEL_VERSION \
-       USE_DEFAULT_KERNEL_CONFIG=$USE_DEFAULT_KERNEL_CONFIG \
-       $(cat $BUILD_DIR/diskinfo) \
-       DISK_DEVICE=$(losetup -j $BUILD_DIR/$VDISK_FILENAME | cut -d ':' -f1) \
-       /bin/bash --login $chroot_script
+    chroot "$LFS" /usr/bin/env -i \
+	   HOME=/root \
+	   TERM="$TERM" \
+	   PS1='(lfs chroot) \u:\w\$ ' \
+	   PATH=/usr/bin:/usr/sbin \
+	   MAKEFLAGS=$MAKEFLAGS \
+	   USE_UEFI=$USE_UEFI \
+	   FERNINUX_TARGET_ARCH=$FERNINUX_TARGET_ARCH \
+	   INSTALLED_PACKAGES_FILE=$INSTALLED_PACKAGES_FILE \
+	   FERNINUX_KERNEL_VERSION=$FERNINUX_KERNEL_VERSION \
+	   USE_DEFAULT_KERNEL_CONFIG=$USE_DEFAULT_KERNEL_CONFIG \
+	   $(cat $BUILD_DIR/diskinfo) \
+	   DISK_DEVICE=$(losetup -j $BUILD_DIR/$VDISK_FILENAME | cut -d ':' -f1) \
+	   /bin/bash --login $chroot_script
 
-echo "unmounting virtual filesystem"
-umount -v $LFS/dev/pts
-mountpoint -q $LFS/dev/shm && umount $LFS/dev/shm
-umount -v $LFS/dev
-umount -v $LFS/proc
-umount -v $LFS/sys
-umount -v $LFS/run
+    echo "unmounting virtual filesystem"
+    umount -v $LFS/dev/pts
+    mountpoint -q $LFS/dev/shm && umount $LFS/dev/shm
+    umount -v $LFS/dev
+    umount -v $LFS/proc
+    umount -v $LFS/sys
+    umount -v $LFS/run
 
-umount_devices
+    umount_devices
 
-findmnt | grep $LFS
+    findmnt | grep $LFS
 
+}
