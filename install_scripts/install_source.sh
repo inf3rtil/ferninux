c7843df859c9b9f678c1e6799e7cb437385c788e
Fernando Ferreira Silva <fernando@fer.dev.br>
2025-01-25 01:26:14 -0300

    handle scripts that dont have tar package

--- install_scripts/install_source.sh
+++ install_scripts/install_source.sh
@@ -5,25 +5,30 @@ RECIPES_DIR=$SCRIPT_PATH/recipes/$FERNINUX_TARGET_ARCH
 #TODO: get version from recipes
 #TODO: reinstall flag
 
+if [ ! -f $INSTALLED_PACKAGES_FILE ]; then
+   touch $INSTALLED_PACKAGES_FILE
+fi
+
 function install_package_list() {
     cd $RECIPES_DIR
     for recipe in "${recipes[@]}"
     do
         . ./"$recipe.sh"
 	if [ $(grep -c "$recipe" $INSTALLED_PACKAGES_FILE) -eq 0 ]; then
-	    src_folder=$(tar tf $SOURCES_ROOT_DIR/$SRC_COMPRESSED_FILE | head -1 | sed -e 's/\/.*//')
-	    echo "extracting files from $SRC_COMPRESSED_FILE"
-	    rm -rf $SOURCES_ROOT_DIR/$src_folder
 	    if [[ -v SRC_COMPRESSED_FILE ]]; then
-		tar xvf $SOURCES_ROOT_DIR/$SRC_COMPRESSED_FILE -C $SOURCES_ROOT_DIR
-		cd $SOURCES_ROOT_DIR/$src_folder
+		pushd $SOURCES_ROOT_DIR
+		src_folder=$(tar tf $SRC_COMPRESSED_FILE | head -1 | sed -e 's/\/.*//')
+		echo "extracting files from $SRC_COMPRESSED_FILE"
+		rm -rf $src_folder
+		tar xvf $SRC_COMPRESSED_FILE
+		cd $src_folder
 	    fi
 	    config_source_package
 	    build_source_package
 	    install_source_package
 	    if [[ -v SRC_COMPRESSED_FILE ]]; then
-		rm -rf $SOURCES_ROOT_DIR/$src_folder
-		cd $RECIPES_DIR
+		rm -rf $src_folder
+		popd
 	    fi
 	    echo "$recipe" >> $INSTALLED_PACKAGES_FILE
 	else
