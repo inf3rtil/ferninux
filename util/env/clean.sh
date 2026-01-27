54df4db4448e7d75a12aca5df55ed794f9cf552b
Fernando Ferreira Silva <fernando@fer.dev.br>
2026-01-26 22:21:02 -0300

    clean build

--- util/env/clean.sh
+++ util/env/clean.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-if [[ $ENV_VARS_EXPORTED -ne 1 ]]; then
-    echo "Env variables not found, probable solutions:"
-    echo "1 - source set_env_vars.sh"
-    echo "2 - use sudo -E flag"
-    exit 1
-fi
-
-
 clear_env(){
-    losetup -D
     rm -rfv $BUILD_DIR
     if [[ $KEEP_DOWNLOAD_FILES -eq 0 ]]; then
 	rm -rfv $DOWNLOAD_DIR
     fi
     rm -rfv $BACKUP_DIR
-    userdel $LFS_USER
 }
